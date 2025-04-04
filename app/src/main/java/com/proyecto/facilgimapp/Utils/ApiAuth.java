b013ab1e10d61b5f00effe1410ebd06b266e209f
Fran Santana <santanafran96@gmail.com>
2025-04-04 10:49:41 +0200

    añadido pantalla de login, logout, llamadas a retrofit. Hay que ver porque no abre el teclado en el email

---
+++ app/src/main/java/com/proyecto/facilgimapp/Utils/ApiAuth.java
@@ -0,0 +1,19 @@
+package com.proyecto.facilgimapp.Utils;
+
+
+
+import com.proyecto.facilgimapp.request.LoginRequest;
+import com.proyecto.facilgimapp.response.LoginResponse;
+
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiAuth {
+    @POST("login")
+    Call<LoginResponse> login(@Body LoginRequest loginRequest);
+
+
+
+}
